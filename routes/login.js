e9c456f8fe352a7cc84178f3312fbfa9b6cd5258
RafaelAlvesSantos <rafaelalvesmgs@gmail.com>
2021-01-08 14:47:25 -0300

    connected to database

--- routes/login.js
+++ routes/login.js
@@ -2,5 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).send("Route ok");
+  res.status(200).send("Login page");
 });
+
+router.get("/register", (req, res) => {
+  res.status(200).send("Register page");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 // Session
 const app = express();
+connectDB();
+
+app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
