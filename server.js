ac2349183c48926d1d700f1c94fd2c7fa7254fcb
RafaelAlvesSantos <rafaelalvesmgs@gmail.com>
2021-01-08 13:57:41 -0300

    refactoring without auth0

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require("express");
-const path = require("path");
-const expressSession = require("express-session");
-const passport = require("passport");
-const Auth0Strategy = require("passport-auth0");
 
-require("dotenv").config();
+// Session
 const app = express();
 
 const PORT = process.env.PORT || 3000;
