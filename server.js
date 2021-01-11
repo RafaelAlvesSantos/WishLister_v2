8bb474d5fddc7abd097ee1177ccd58be797d0adc
RafaelAlvesSantos <rafaelalvesmgs@gmail.com>
2021-01-11 15:21:32 -0300

    implemented login functionality in the front-end

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const connectDB = require("./config/db");
+var cors = require("cors");
 
 // Session
 const app = express();
 connectDB();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/api/users", require("./routes/users"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
